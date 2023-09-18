c0999025f43f73f822b7dd40b5f92333eae675a5
Carol Andrade <carol.andradechagas@gmail.com>
2023-09-18 15:19:13 -0300

    component: post

---
+++ src/components/Post.jsx
@@ -0,0 +1,44 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/78247893?v=4"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>Carol Andrade</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="18 de setembro, às 13:29" dateTime="2023-09-18 13:29:00">
+          Publicado a 1 hora
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Olá</p>
+        <p>Acabei de subir um novo projeto no meu GitHub.</p>
+        <p>
+          <a href="https://github.com/carol-andradech">Link</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a> <a href="">#github</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu comentário</strong>
+        <textarea placeholder="Deixe um comentário"></textarea>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
   return (
@@ -8,11 +9,19 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/78247893?v=4"
+          alt=""
+        />
         <strong>Carol</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
